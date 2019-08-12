18f932ab1e93a2734f5c1647a3079f342b47b948
Matteo Muscas <thatman.j@gmail.com>
2019-08-12 08:47:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   #helper_method :current_user
   #before_action :require_logged_in, except: [:new, :create, :home]
+
+  def current_cart
+    if user_signed_in?
+      Cart.find_or_create_by(user_id: current_user.id)
+    else
+      nil
+    end
+  end
+  helper_method :current_cart
 end
