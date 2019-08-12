257da2ae5189582f5fff5c9a609006df129b47e5
Matteo Muscas <thatman.j@gmail.com>
2019-08-12 08:31:54 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
 
   has_many :carts
   belongs_to :current_cart, class_name: 'Cart'
