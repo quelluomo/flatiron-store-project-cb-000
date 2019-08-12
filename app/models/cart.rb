bedb1c8b6e14478fc5a453a8de78d1f538b16d21
Matteo Muscas <thatman.j@gmail.com>
2019-08-12 09:20:34 +0000

    Automatically backed up by Learn

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,7 +4,11 @@ class Cart < ActiveRecord::Base
   has_many :items, through: :line_items
 
   def total
-    self.line_items.sum {|line_item| line_item.quantity * line_item.item.price}
+    total = 0
+    self.line_items.each do |line_item|
+      total += line_item.item.price * line_item.quantity
+    end
+    return total
   end
 
   def add_item(item_id)
